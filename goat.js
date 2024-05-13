6d8623436c18174f5c073c58581d5858ad084a69
GOAT-Shinigami <cristiano.manuel.dias@gmail.com>
2024-05-14 00:16:20 +0100

    Update goat.js

--- goat.js
+++ goat.js
@@ -1,29 +1,30 @@
-function adjustStylesBasedOnScreen() {
+function adjustBackgroundImage() {
     const headerBg = document.querySelector('.header-bg');
-    const screenWidth = window.innerWidth;
+    const aspectRatio = window.innerWidth / window.innerHeight;
 
-    // Ajustar a altura do cabeçalho com base no tamanho da tela
-    if (screenWidth > 1500) { // Muito grande
-        headerBg.style.minHeight = '768px';
-        headerBg.style.backgroundAttachment = 'fixed';
-    } else if (screenWidth > 1200) { // Grande
-        headerBg.style.minHeight = '600px';
-        headerBg.style.backgroundAttachment = 'scroll';
-    } else if (screenWidth > 992) { // Médio
-        headerBg.style.minHeight = '500px';
-        headerBg.style.backgroundAttachment = 'scroll';
-    } else { // Pequeno e muito pequeno
-        headerBg.style.minHeight = '300px'; // Usar o valor padrão de CSS
+    // Define a estratégia de tamanho de fundo com base na relação de aspecto
+    if (aspectRatio > 1) {
+        // Telas mais largas que altas (desktops)
+        headerBg.style.backgroundSize = (aspectRatio > 1.77) ? 'contain' : 'cover';
+    } else {
+        // Telas mais altas que largas (smartphones)
+        headerBg.style.backgroundSize = 'contain'; // Evitar cortar partes importantes em dispositivos verticais
     }
 
-    // Verificações específicas para navegadores, se necessário
-    var ua = navigator.userAgent;
-    if (/MSIE \d|Trident.*rv:/.test(ua)) {
-        // Código específico para Internet Explorer
-        headerBg.style.backgroundSize = 'contain';
+    // Aplica uma altura mínima com base na largura da tela
+    if (window.innerWidth > 1500) {
+        headerBg.style.minHeight = '768px'; // Grandes desktops
+    } else if (window.innerWidth > 1200) {
+        headerBg.style.minHeight = '600px'; // Desktops médios
+    } else if (window.innerWidth > 992) {
+        headerBg.style.minHeight = '500px'; // Tablets grandes e laptops pequenos
+    } else if (window.innerWidth > 768) {
+        headerBg.style.minHeight = '450px'; // Tablets
+    } else {
+        headerBg.style.minHeight = '300px'; // Smartphones
     }
 }
 
-// Adiciona event listeners para carregamento e redimensionamento
-window.addEventListener('load', adjustStylesBasedOnScreen);
-window.addEventListener('resize', adjustStylesBasedOnScreen);
+// Adicionar listeners para carregamento e redimensionamento de janela
+window.addEventListener('load', adjustBackgroundImage);
+window.addEventListener('resize', adjustBackgroundImage);
