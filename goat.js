160d3ac90a83625dd27e50f2754011e63d208a17
GOAT-Shinigami <cristiano.manuel.dias@gmail.com>
2024-05-15 10:35:22 +0100

    Update goat.js

--- goat.js
+++ goat.js
@@ -24,26 +24,26 @@ function adjustBackgroundAndNavbar() {
     // Ajustes dinâmicos para links na navbar
     const links = navbar.querySelectorAll('.nav-link');
     links.forEach(link => {
-        link.style.fontSize = (window.innerWidth > 1200) ? '1.4rem' : (window.innerWidth > 992) ? '1.1rem' : (window.innerWidth > 768) ? '1rem' : '0.9rem';
+        link.style.fontSize = (window.innerWidth > 1200) ? '1.4rem' : (window.innerWidth > 992) ? '1rem' : (window.innerWidth > 768) ? '0.9rem' : '0.8rem';
         link.style.marginRight = (window.innerWidth > 1200) ? '0.625rem' : '0.3rem';
     });
 
     // Ajustes dinâmicos para o botão Connect Wallet
     const connectWalletBtn = navbar.querySelector('.connect-wallet');
     if (window.innerWidth > 1200) {
-        connectWalletBtn.style.fontSize = '1rem';
-    } else if (window.innerWidth > 992) {
         connectWalletBtn.style.fontSize = '0.9rem';
+    } else if (window.innerWidth > 992) {
+        connectWalletBtn.style.fontSize = '0.8rem';
     } else if (window.innerWidth > 768) {
         connectWalletBtn.style.fontSize = '0.8rem';
     } else {
         connectWalletBtn.style.fontSize = '0.7rem';
-        connectWalletBtn.style.marginLeft = '0'; // Remover margem esquerda em dispositivos móveis
     }
 }
 
 window.addEventListener('load', adjustBackgroundAndNavbar);
 window.addEventListener('resize', adjustBackgroundAndNavbar);
+
 
 
 
