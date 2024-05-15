ffcff2d6758272972ec014488e449164ff39780d
GOAT-Shinigami <cristiano.manuel.dias@gmail.com>
2024-05-15 23:34:30 +0100

    Update goat.js

--- goat.js
+++ goat.js
@@ -10,10 +10,13 @@ function toggleNav() {
 // Função para alternar a visibilidade do conteúdo do disclosure
 function toggleDisclosure() {
     var x = document.getElementById("disclosure-content");
+    console.log("toggleDisclosure called"); // Linha de depuração
     if (x.classList.contains("w3-hide")) {
         x.classList.remove("w3-hide");
+        console.log("Showing disclosure content"); // Linha de depuração
     } else {
         x.classList.add("w3-hide");
+        console.log("Hiding disclosure content"); // Linha de depuração
     }
 }
 
